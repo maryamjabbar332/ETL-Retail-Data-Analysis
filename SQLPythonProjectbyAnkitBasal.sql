986ec64d583ea4059bf44555316e1da302a9178f
maryamjabbar332 <91887874+maryamjabbar332@users.noreply.github.com>
2024-09-30 07:21:06 +0100

    Update SQLPythonProjectbyAnkitBasal.sql

--- SQLPythonProjectbyAnkitBasal.sql
+++ SQLPythonProjectbyAnkitBasal.sql
@@ -36,7 +36,7 @@ select product_id, sum(sale_price) as sales
 from df_orders
 group by product_id
 order by sales desc                      -- first we need to find the sales against all the product_id
-											-- and after running the initial query we can add top 10 
+					 -- and after running the initial query we can add top 10 
 
 
 select top 10 product_id, sum(sale_price) as sales
@@ -69,7 +69,7 @@ sum(sale_price) as sales
 from df_orders
 group by year(order_date), month(order_date)
 --order by year(order_date), month(order_date)
-)											    --order by is invalid inside cte
+)						   --order by is invalid inside cte
 select order_month --order_year                   -- after we agregated the case statements we don't need order year
 ,sum(case when order_year=2022 then sales else 0 end) as sales_2022        --case 1: one column will generate against this case statement
 ,sum(case when order_year=2023 then sales else 0 end) as sales_2023        -- case 2: second column of sales_2023 will be generated against this case statement
@@ -105,12 +105,12 @@ select sub_category, year(order_date) as order_year,
 sum(sale_price) as sales
 from df_orders
 group by sub_category, year(order_date)
---order by year(order_date), month(order_date)              --order by is invalid inside cte
+--order by year(order_date), month(order_date)                
 )
 , cte2 as (                                              
-select sub_category --order_year                   -- after we agregated the case statements we don't need order year
-,sum(case when order_year=2022 then sales else 0 end) as sales_2022        --case 1: one column will generate against this case statement
-,sum(case when order_year=2023 then sales else 0 end) as sales_2023        -- case 2: second column of sales_2023 will be generated against this case statement
+select sub_category                   
+,sum(case when order_year=2022 then sales else 0 end) as sales_2022        
+,sum(case when order_year=2023 then sales else 0 end) as sales_2023       
 from cte
 group by sub_category
 )
